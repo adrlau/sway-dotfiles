a74d4948ded1177711fbe17dff3a9aabbb9eb50d
= <=>
2023-09-11 00:28:45 +0200

    small changes

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+
 dir=/home/gunalx/.config/sway/Wallpapers 
 swaybg -i $(find $dir -type f | shuf -n 1) -m fill &&
 
 OLD_PID=$!
 while true; do
-    sleep 3000
+    sleep 300
     swaybg -i $(find $dir -type f | shuf -n1) -m fill &
     NEXT_PID=$!
     sleep 5
