3d07e788991739024784907f6f5a29052a7bda9e
Adrian Gunnar Lauterer <adrian@lauterer.it>
2024-02-12 20:23:37 +0100

    current config

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-
-dir=/home/gunalx/.config/sway/Wallpapers 
-swaybg -i $(find $dir -type f | shuf -n 1) -m fill &&
-
+dir=/home/gunalx/.config/sway/Wallpapers/.
+#swaybg -i $(find $dir -type f | shuf -n 1) -m fill &&
+#OLD_PID=$!
+#while true; do
+#    sleep 10
+#    swaybg -i $(find $dir -type f | shuf -n 1) -m fill &&
+#    NEXT_PID=$!
+#    sleep 5
+#    kill $OLD_PID
+#    OLD_PID=$NEXT_PID
+#done
+swaybg -i $(find $dir -type f | shuf -n1) -m fill &
 OLD_PID=$!
 while true; do
-    sleep 10
+    sleep 60
     swaybg -i $(find $dir -type f | shuf -n1) -m fill &
     NEXT_PID=$!
     sleep 5
